7135510db25f496716ef8df5c0fd62ef2983c208
Rakesh.P.D <errakeshpd@gmail.com>
2014-07-25 11:21:54 +0530

    views added and controller in dex created - index page altered

--- app/controllers/NerdController.php
+++ app/controllers/NerdController.php
@@ -9,7 +9,12 @@ class NerdController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
+        // get all the nerds
+         $nerds = Nerd::all();
+    
+        // load the view and pass the nerds
+           return View::make('nerds.index')
+                  ->with('nerds', $nerds);
 	}
 
 
